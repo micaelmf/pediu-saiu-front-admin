40f012a5893a75641b0464b2812be53819fe0a1a
Micael Ferreira <micaelmf@gmail.com>
2023-09-17 19:59:21 -0300

    feat: add partials

--- server.js
+++ server.js
@@ -6,10 +6,21 @@ const app = express();
 const port = 3002;
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'src'));
+app.set('views', path.join(__dirname, 'src/views'));
 
-app.get("/message", (_, res) => {
-    res.render("message")
+app.get("/", (_, res) => {
+    const page = {
+        title: 'Produtos'
+    }
+    res.render("blank-page", page)
+});
+
+app.get("/produtos", (_, res) => {
+    const page = {
+        title: 'Produtos'
+    }
+
+    res.render("products", page)
 });
 
 viteExpress.listen(app, port, () => console.log("Server is listening on port " + port));
